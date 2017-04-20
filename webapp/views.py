3fb125fb9a778b98c86fd1147d756db3c5ba42d1
Bruno Takahashi <bruno.kazuo90@gmail.com>
2017-04-20 19:22:07 -0300

    Render variables to template

--- webapp/views.py
+++ webapp/views.py
@@ -8,3 +8,5 @@ def index(request):
     # Render always look for a folder called templates
     return render(request, 'webapp/home.html')
 
+def contact(request):
+    return render(request, 'webapp/basic.html', {'content': ['If you would like to contact me, plase email me','teste@teste.com']}) # Passing a dict to the render html file
