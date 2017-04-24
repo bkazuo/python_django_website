a491fc76d26dd64afc8d25f61f3ac2d610d1a52f
Bruno Takahashi <bruno.kazuo90@gmail.com>
2017-04-24 13:24:54 -0300

    Small fixes and improved blog list layout

--- python_django_website/urls.py
+++ python_django_website/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('webapp.urls')),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^', include('blog.urls')),
 ]
--- webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views # Import from the very app
-
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^contact/', views.contact, name='contact'),
-]
